c23b9043a1dede974230e4dc50a0801192fc93a0
francis85a <franperezcid85@gmail.com>
2026-04-08 10:07:09 +0200

    build(Arnold_enum): Crear proyecto

---
+++ src/main/java/edu/teamrocket/arnold_enum/App.java
@@ -0,0 +1,10 @@
+package edu.teamrocket.arnold_enum;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
